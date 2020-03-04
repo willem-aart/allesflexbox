8c1da3aa11c922a38ed3c76cc9e56f63e3aa5b0e
Willem-Aart van Dorpen <mail@willem-aart.nl>
2020-03-04 19:54:30 +0100

    add some puppeteer code

--- api/foobar.ts
+++ api/foobar.ts
@@ -1,5 +1,12 @@
 import { NowRequest, NowResponse } from "@now/node";
+import puppeteer from "puppeteer";
 
-export default (req: NowRequest, res: NowResponse) => {
-  res.json({ name: "John Doe", email: "johndoe@example.com" });
+export default async (req: NowRequest, res: NowResponse) => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto("https://example.com");
+  const viewport = await page.viewport();
+  await browser.close();
+
+  res.json(viewport);
 };
