2c55737adf6bd13f01f98b8ed393e15397a5ce56
Willem-Aart van Dorpen <mail@willem-aart.nl>
2020-03-04 20:02:12 +0100

    update browser setup

--- api/foobar.ts
+++ api/foobar.ts
@@ -1,12 +1,17 @@
 import { NowRequest, NowResponse } from "@now/node";
-import puppeteer from "puppeteer";
+import chrome from "chrome-aws-lambda";
+import puppeteer from "puppeteer-core";
 
 export default async (req: NowRequest, res: NowResponse) => {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({
+    args: chrome.args,
+    executablePath: await chrome.executablePath,
+    headless: chrome.headless
+  });
+
   const page = await browser.newPage();
   await page.goto("https://example.com");
   const viewport = await page.viewport();
   await browser.close();
-
   res.json(viewport);
 };
