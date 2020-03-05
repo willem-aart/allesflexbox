43b2e1727079fa50dfedee9d1ca4fb447bcf4d1f
Willem-Aart van Dorpen <willem-aart@Willem-Aarts-MacBook-Pro.local>
2020-03-05 09:38:36 +0100

    set element border

--- api/screenshot.ts
+++ api/screenshot.ts
@@ -16,48 +16,48 @@ export default async (request: NowRequest, response: NowResponse) => {
     waitUntil: "networkidle0"
   });
 
-  // await page.evaluate(() => {
-  //   const elements = document.getElementsByTagName("*");
+  await page.evaluate(() => {
+    const elements = document.getElementsByTagName("*");
 
-  //   return [...elements]
-  //     .filter(
-  //       element =>
-  //         ![
-  //           "html",
-  //           "head",
-  //           "meta",
-  //           "title",
-  //           "link",
-  //           "script",
-  //           "style",
-  //           "noscript"
-  //         ].includes(element.tagName.toLowerCase())
-  //     )
-  //     .forEach(element => {
-  //       const isFlexContainer = (element: Element) =>
-  //         window.getComputedStyle(element).display === "flex" ||
-  //         window.getComputedStyle(element).display === "inline-flex";
+    return [...elements]
+      .filter(
+        element =>
+          ![
+            "html",
+            "head",
+            "meta",
+            "title",
+            "link",
+            "script",
+            "style",
+            "noscript"
+          ].includes(element.tagName.toLowerCase())
+      )
+      .forEach(element => {
+        const isFlexContainer = (element: Element) =>
+          window.getComputedStyle(element).display === "flex" ||
+          window.getComputedStyle(element).display === "inline-flex";
 
-  //       const ancestors = (function getAncestors(
-  //         element: Element,
-  //         ancestors: Element[] = []
-  //       ): Element[] {
-  //         return element.parentElement === null ||
-  //           element.parentElement.tagName.toLowerCase() === "html"
-  //           ? ancestors
-  //           : [element.parentElement, ...getAncestors(element.parentElement)];
-  //       })(element);
+        const ancestors = (function getAncestors(
+          element: Element,
+          ancestors: Element[] = []
+        ): Element[] {
+          return element.parentElement === null ||
+            element.parentElement.tagName.toLowerCase() === "html"
+            ? ancestors
+            : [element.parentElement, ...getAncestors(element.parentElement)];
+        })(element);
 
-  //       if (
-  //         !isFlexContainer(element) &&
-  //         element.parentElement !== null &&
-  //         isFlexContainer(element.parentElement) &&
-  //         ancestors.some(isFlexContainer)
-  //       ) {
-  //         // ...
-  //       }
-  //     });
-  // });
+        if (
+          !isFlexContainer(element) &&
+          element.parentElement !== null &&
+          !isFlexContainer(element.parentElement) &&
+          !ancestors.some(isFlexContainer)
+        ) {
+          element.style.border = "3px solid lightgreen";
+        }
+      });
+  });
 
   const file = await page.screenshot({ fullPage: true, type: "jpeg" });
   response.statusCode = 200;
